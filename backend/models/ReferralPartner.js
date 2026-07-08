1cf40d12a13d2a3e97d6ae1263cf9d1f1b3d06a6
vishalyadavvv <vishalkumaryadav735@gmail.com>
2026-07-08 10:54:15 +0530

    link

--- backend/models/ReferralPartner.js
+++ backend/models/ReferralPartner.js
@@ -49,8 +49,7 @@ const referralPartnerSchema = new mongoose.Schema({
   referralCode: {
     type: String,
     unique: true,
-    sparse: true,
-    default: null
+    sparse: true
   },
   referralLink: {
     type: String,
